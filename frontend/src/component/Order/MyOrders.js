18ce90e9d3b06392929b127ed7a063d1a0bb647c
Saad Amin <saadamin662@gmail.com>
2023-12-06 21:50:47 +0500

    working on MyOrders

--- frontend/src/component/Order/MyOrders.js
+++ frontend/src/component/Order/MyOrders.js
@@ -15,65 +15,6 @@ const MyOrders = () => {
     const { loading, error, orders } = useSelector((state) => state.myOrders);
     const { user } = useSelector((state) => state.user);
 
-    // const columns = [
-    //     { field: "id", headerName: "Order ID", minWidth: 300, flex: 1 },
-
-    //     {
-    //         field: "status",
-    //         headerName: "Status",
-    //         minWidth: 150,
-    //         flex: 0.5,
-    //         cellClassName: (params) => {
-    //             return params.getValue(params.id, "status") === "Delivered"
-    //                 ? "greenColor"
-    //                 : "redColor";
-    //         },
-    //     },
-    //     {
-    //         field: "itemsQty",
-    //         headerName: "Items Qty",
-    //         type: "number",
-    //         minWidth: 150,
-    //         flex: 0.3,
-    //     },
-
-    //     {
-    //         field: "amount",
-    //         headerName: "Amount",
-    //         type: "number",
-    //         minWidth: 270,
-    //         flex: 0.5,
-    //     },
-
-    //     {
-    //         field: "actions",
-    //         flex: 0.3,
-    //         headerName: "Actions",
-    //         minWidth: 150,
-    //         type: "number",
-    //         sortable: false,
-    //         renderCell: (params) => {
-    //             return (
-    //                 <Link to={`/order/${params.getValue(params.id, "id")}`}>
-    //                     <RightOutlined />
-    //                 </Link>
-    //             );
-    //         },
-    //     },
-    // ];
-
-
-    // const rows = orders
-    //     ? orders.map((item) => ({
-    //         key: item._id,
-    //         id: item._id,
-    //         status: item.orderStatus,
-    //         itemsQty: item.OrderItems.length,
-    //         amount: item.totalPrice,
-    //     }))
-    //     : [];
-
-
     useEffect(() => {
         if (error) {
             toast.error(error);
