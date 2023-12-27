01007a9b100808cd9626a233a065a1e8d3441c36
Saad Amin <saadamin662@gmail.com>
2023-12-27 20:16:00 +0500

    working on OrderSuccess

--- frontend/src/component/Cart/OrderSuccess.js
+++ frontend/src/component/Cart/OrderSuccess.js
@@ -1,15 +1,27 @@
 import React from "react";
-import CheckCircleFilled from "@ant-design/icons";
-import "./OrderSuccess.css";
-import { Typography } from "antd";
+import { useSelector } from "react-redux";
+import CheckCircleFilled from "@ant-design/icons/CheckCircleFilled";
+import { Typography, Button } from "antd";
 import { Link } from "react-router-dom";
+import "./OrderSuccess.css";
 
 const OrderSuccess = () => {
+  const { user } = useSelector((state) => state.user);
   return (
     <div className="orderSuccess">
-      <CheckCircleFilled />
-      <Typography>Your Order has been Placed successfully </Typography>
-      <Link to="/orders">View Orders</Link>
+      <CheckCircleFilled className="successIcon" />
+      <Typography.Title level={3} className="successTitle">
+        {`Thank you, ${user.name}! Your Order has been Placed Successfully`}
+      </Typography.Title>
+      <Typography.Text className="successText">
+        Thank you for shopping with us! Your order is confirmed, and we're
+        preparing it for shipment.
+      </Typography.Text>
+      <Link to="/orders">
+        <Button type="primary" className="viewOrdersBtn">
+          View Orders
+        </Button>
+      </Link>
     </div>
   );
 };
