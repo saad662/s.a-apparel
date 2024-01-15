25a0332afa14f7d2775a9bdfc88475464cac6add
Saad Amin <saadamin662@gmail.com>
2024-01-15 19:20:12 +0500

    working on image extender

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,9 @@ const app = express();
 // Load environment variables from .env file
 dotenv.config({ path: "backend/config/.env" });
 
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
